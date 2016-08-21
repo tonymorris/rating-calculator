05c10bcfda82a21d76ad19b1bd866af8e56b9a2f
Joshua Morris <joshuajohnmorris@gmail.com>
2016-08-21 10:08:08 +1000

    transformed rating

--- calculator.hs
+++ calculator.hs
@@ -1,6 +1,6 @@
-transformPlayer :: Integer -> Double
-transformPlayer x =
+transformPlayer :: Int -> Int
+transformPlayer x = 10 ^ (x `div` 400)
 
 
---transformOpponent :: Integer -> Double
---transformOpponent
+transformOpponent :: Int -> Int
+transformOpponent y = 10 ^ (y `div` 400)
