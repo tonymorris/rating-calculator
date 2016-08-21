e62923a0cc34b38df0aec5bebff332fb063bb085
Joshua Morris <joshuajohnmorris@gmail.com>
2016-08-21 18:32:51 +1000

    expected scores

--- calculator.hs
+++ calculator.hs
@@ -1,9 +1,15 @@
-transformPlayer :: Integer -> Integer
-transformPlayer x = 10 ^ (x `div` 400)
+playerTransformation :: Integer -> Integer
+playerTransformation x = 10 ^ (x `div` 400)
 
 
-transformOpponent :: Integer -> Integer
-transformOpponent y = 10 ^ (y `div` 400)
+opponentTransformation :: Integer -> Integer
+opponentTransformation y = 10 ^ (y `div` 400)
 
 expectedPlayer :: Integer -> Integer -> Integer
-expectedPlayer x y = transformPlayer x `div` (transformPlayer x + transformOpponent y)
+expectedPlayer x y = 
+	playerTransformation x `div` (playerTransformation x + opponentTransformation y)
+
+expectedOpponent :: Integer -> Integer -> Integer
+expectedOpponent x y = 
+	opponentTransformation x `div` (playerTransformation x + opponentTransformation y)
+
