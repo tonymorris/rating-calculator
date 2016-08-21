a2feffd41a06c4e14cd6552f8f7cc6dba0a9782b
Joshua Morris <joshuajohnmorris@gmail.com>
2016-08-21 18:29:33 +1000

    expected player

--- calculator.hs
+++ calculator.hs
@@ -1,6 +1,9 @@
-transformPlayer :: Int -> Int
+transformPlayer :: Integer -> Integer
 transformPlayer x = 10 ^ (x `div` 400)
 
 
-transformOpponent :: Int -> Int
+transformOpponent :: Integer -> Integer
 transformOpponent y = 10 ^ (y `div` 400)
+
+expectedPlayer :: Integer -> Integer -> Integer
+expectedPlayer x y = transformPlayer x `div` (transformPlayer x + transformOpponent y)
